133454a3455dc340051fbb04622e3dc8df94fbb7
Kuba Werłos <werlos@gmail.com>
2019-08-02 20:09:40 +0200

    Add CommentSurroundedBySpacesFixer

---
+++ src/Fixer/CommentSurroundedBySpacesFixer.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace PhpCsFixerCustomFixers\Fixer;
+
+use PhpCsFixer\FixerDefinition\CodeSample;
+use PhpCsFixer\FixerDefinition\FixerDefinition;
+use PhpCsFixer\FixerDefinition\FixerDefinitionInterface;
+use PhpCsFixer\Preg;
+use PhpCsFixer\Tokenizer\Token;
+use PhpCsFixer\Tokenizer\Tokens;
+
+final class CommentSurroundedBySpacesFixer extends AbstractFixer
+{
+    public function getDefinition(): FixerDefinitionInterface
+    {
+        return new FixerDefinition(
+            'comment must be surrounded by spaces',
+            [new CodeSample('<?php
+/*foo*/
+')]
+        );
+    }
+
+    public function isCandidate(Tokens $tokens): bool
+    {
+        return $tokens->isAnyTokenKindsFound([T_COMMENT, T_DOC_COMMENT]);
+    }
+
+    public function isRisky(): bool
+    {
+        return false;
+    }
+
+    public function fix(\SplFileInfo $file, Tokens $tokens): void
+    {
+        foreach ($tokens as $index => $token) {
+            if (!$token->isGivenKind([T_COMMENT, T_DOC_COMMENT])) {
+                continue;
+            }
+
+            $newContent = Preg::replace('/^(\/\/|#|\/\*+)((?!(?:\/|\*| )).+)$/', '$1 $2', $token->getContent());
+            $newContent = Preg::replace('/^(.+(?<!(?:\/|\*| )))(\*+\/)$/', '$1 $2', $newContent);
+
+            if ($newContent === $token->getContent()) {
+                continue;
+            }
+
+            $tokens[$index] = new Token([$token->getId(), $newContent]);
+        }
+    }
+
+    public function getPriority(): int
+    {
+        // Must be run after MultilineCommentOpeningClosingFixer
+        return -1;
+    }
+}
