66e850af23f49ca4611e6d23ba3e19d4ddc5a2ee
Kuba Werłos <werlos@gmail.com>
2019-08-13 06:06:10 +0200

    Add NoDuplicatedImportsFixer

---
+++ src/Fixer/NoDuplicatedImportsFixer.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace PhpCsFixerCustomFixers\Fixer;
+
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use PhpCsFixer\FixerDefinition\CodeSample;
+use PhpCsFixer\FixerDefinition\FixerDefinition;
+use PhpCsFixer\FixerDefinition\FixerDefinitionInterface;
+use PhpCsFixer\Tokenizer\Analyzer\Analysis\NamespaceUseAnalysis;
+use PhpCsFixer\Tokenizer\Analyzer\NamespacesAnalyzer;
+use PhpCsFixer\Tokenizer\Analyzer\NamespaceUsesAnalyzer;
+use PhpCsFixer\Tokenizer\Tokens;
+
+final class NoDuplicatedImportsFixer extends AbstractFixer
+{
+    public function getDefinition(): FixerDefinitionInterface
+    {
+        return new FixerDefinition(
+            'Duplicated `use` statements must be removed.',
+            [new CodeSample('<?php
+namespace FooBar;
+use Foo;
+use Foo;
+use Bar;
+')]
+        );
+    }
+
+    public function isCandidate(Tokens $tokens): bool
+    {
+        return $tokens->isTokenKindFound(T_USE);
+    }
+
+    public function isRisky(): bool
+    {
+        return false;
+    }
+
+    public function fix(\SplFileInfo $file, Tokens $tokens): void
+    {
+        $useDeclarations = (new NamespaceUsesAnalyzer())->getDeclarationsFromTokens($tokens);
+
+        foreach ((new NamespacesAnalyzer())->getDeclarations($tokens) as $namespace) {
+            $currentNamespaceUseDeclarations = \array_filter(
+                $useDeclarations,
+                static function (NamespaceUseAnalysis $useDeclaration) use ($namespace) {
+                    return $useDeclaration->getStartIndex() >= $namespace->getScopeStartIndex()
+                        && $useDeclaration->getEndIndex() <= $namespace->getScopeEndIndex();
+                }
+            );
+
+            $used = [];
+
+            foreach ($currentNamespaceUseDeclarations as $useDeclaration) {
+                if (isset($used[$useDeclaration->getFullName()])) {
+                    $this->removeUseDeclaration($tokens, $useDeclaration);
+                }
+                $used[$useDeclaration->getFullName()] = true;
+            }
+        }
+    }
+
+    public function getPriority(): int
+    {
+        return 0;
+    }
+
+    private function removeUseDeclaration(Tokens $tokens, NamespaceUseAnalysis $useDeclaration): void
+    {
+        static $noUnusedImportsFixer, $reflectionMethod;
+
+        if ($reflectionMethod === null) {
+            $noUnusedImportsFixer = new NoUnusedImportsFixer();
+
+            $reflectionMethod = new \ReflectionMethod($noUnusedImportsFixer, 'removeUseDeclaration');
+            $reflectionMethod->setAccessible(true);
+        }
+
+        $reflectionMethod->invoke($noUnusedImportsFixer, $tokens, $useDeclaration);
+    }
+}
---
+++ tests/Fixer/NoDuplicatedImportsFixerTest.php
@@ -0,0 +1,123 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Tests\Fixer;
+
+/**
+ * @internal
+ *
+ * @covers \PhpCsFixerCustomFixers\Fixer\NoDuplicatedImportsFixer
+ */
+final class NoDuplicatedImportsFixerTest extends AbstractFixerTestCase
+{
+    public function testPriority(): void
+    {
+        static::assertSame(0, $this->fixer->getPriority());
+    }
+
+    public function testIsRisky(): void
+    {
+        static::assertFalse($this->fixer->isRisky());
+    }
+
+    /**
+     * @param string      $expected
+     * @param null|string $input
+     *
+     * @dataProvider provideFixCases
+     */
+    public function testFix(string $expected, ?string $input = null): void
+    {
+        $this->doTest($expected, $input);
+    }
+
+    public function provideFixCases(): iterable
+    {
+        yield [
+            '<?php
+                namespace FooBar;
+                use Foo;
+                use Bar;
+            ',
+            '<?php
+                namespace FooBar;
+                use Foo;
+                use Foo;
+                use Bar;
+            ',
+        ];
+
+        yield [
+            '<?php
+                namespace FooBar;
+                use Foo;
+                use Bar;
+            ',
+            '<?php
+                namespace FooBar;
+                use Foo;
+                use Bar;
+                use Foo;
+            ',
+        ];
+
+        yield [
+            '<?php
+                namespace FooBar;
+                use Vendor\Project\Foo;
+                use Bar;
+                use Baz;
+            ',
+            '<?php
+                namespace FooBar;
+                use Vendor\Project\Foo;
+                use Bar;
+                use Vendor\Project\Foo;
+                use Baz;
+            ',
+        ];
+
+        yield [
+            '<?php
+                namespace FooBar;
+                use Vendor\Project\Duplicated\Foo;
+                use Vendor\Foo;
+                use Vendor\Project\Duplicated\Bar;
+                use Vendor\Bar;
+            ',
+            '<?php
+                namespace FooBar;
+                use Vendor\Project\Duplicated\Foo;
+                use Vendor\Foo;
+                use Vendor\Project\Duplicated\Bar;
+                use Vendor\Project\Duplicated\Foo;
+                use Vendor\Project\Duplicated\Bar;
+                use Vendor\Bar;
+                use Vendor\Project\Duplicated\Foo;
+                use Vendor\Project\Duplicated\Foo;
+            ',
+        ];
+
+        yield [
+            '<?php
+                namespace Foo;
+                use Vendor\Class1;
+                use Vendor\Class2;
+                namespace Bar;
+                use Vendor\Class1;
+                use Vendor\Class2;
+            ',
+            '<?php
+                namespace Foo;
+                use Vendor\Class1;
+                use Vendor\Class1;
+                use Vendor\Class2;
+                namespace Bar;
+                use Vendor\Class1;
+                use Vendor\Class2;
+                use Vendor\Class2;
+            ',
+        ];
+    }
+}
