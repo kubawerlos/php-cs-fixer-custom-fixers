14dac3db1e17e8546dfc4fe1e690d592b2a81a2d
Kuba Werłos <werlos@gmail.com>
2020-08-15 21:01:07 +0200

    Update

--- dev-tools/check_file_permissions.sh
+++ dev-tools/check_file_permissions.sh
@@ -15,7 +15,7 @@ then
     printf '\033[97;41mWrong permissions detected:\033[0m\n'
     e=$(printf '\033')
     echo "${files_with_wrong_permissions}"
-    exit 3
+    exit 1
 fi
 
 printf '\033[0;32mNo wrong permissions detected.\033[0m\n'
