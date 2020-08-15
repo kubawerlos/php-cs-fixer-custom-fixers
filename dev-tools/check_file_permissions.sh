940f8b23cba1a1344b96b2c81c1064a72bdc2715
Kuba Werłos <9282069+kubawerlos@users.noreply.github.com>
2020-08-15 21:09:21 +0200

    Check file permissions (#356)

---
+++ dev-tools/check_file_permissions.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -eu
+
+files_with_wrong_permissions=$(
+    git ls-files --stage . \
+        ':!*.sh' \
+        ':!dev-tools/build-infection-config' \
+        ':!dev-tools/readme' \
+    | grep '100755 ' \
+    | sort -fh
+)
+
+if [ "$files_with_wrong_permissions" ]
+then
+    printf '\033[97;41mWrong permissions detected:\033[0m\n'
+    e=$(printf '\033')
+    echo "${files_with_wrong_permissions}"
+    exit 1
+fi
+
+printf '\033[0;32mNo wrong permissions detected.\033[0m\n'
