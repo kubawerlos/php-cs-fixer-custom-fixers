448e80d9c1c287d50d10de7fe47a13d9945647e3
Kuba Werłos <werlos@gmail.com>
2021-10-05 16:45:53 +0200

    Restore SwitchAnalyzer

---
+++ src/Analyzer/Analysis/SwitchAnalysis.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of PHP CS Fixer: custom fixers.
+ *
+ * (c) 2018 Kuba Werłos
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace PhpCsFixerCustomFixers\Analyzer\Analysis;
+
+/**
+ * @internal
+ */
+final class SwitchAnalysis
+{
+    /** @var int */
+    private $casesStart;
+
+    /** @var int */
+    private $casesEnd;
+
+    /** @var array<CaseAnalysis> */
+    private $cases = [];
+
+    /**
+     * @param array<CaseAnalysis> $cases
+     */
+    public function __construct(int $casesStart, int $casesEnd, array $cases)
+    {
+        $this->casesStart = $casesStart;
+        $this->casesEnd = $casesEnd;
+        $this->cases = $cases;
+    }
+
+    public function getCasesStart(): int
+    {
+        return $this->casesStart;
+    }
+
+    public function getCasesEnd(): int
+    {
+        return $this->casesEnd;
+    }
+
+    /**
+     * @return array<CaseAnalysis>
+     */
+    public function getCases(): array
+    {
+        return $this->cases;
+    }
+}
