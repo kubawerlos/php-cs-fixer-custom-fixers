721a11e17b6442364632839497aa12a78aedfeda
Kuba Werłos <werlos@gmail.com>
2021-10-06 17:02:11 +0200

    Add ConstructorAnalysis

---
+++ src/Analyzer/Analysis/ConstructorAnalysis.php
@@ -0,0 +1,176 @@
+<?php
+
+/*
+ * This file is part of PHP CS Fixer: custom fixers.
+ *
+ * (c) 2018 Kuba Werłos
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace PhpCsFixerCustomFixers\Analyzer\Analysis;
+
+use PhpCsFixer\Tokenizer\CT;
+use PhpCsFixer\Tokenizer\Tokens;
+
+/**
+ * @internal
+ */
+final class ConstructorAnalysis
+{
+    /** @var Tokens */
+    private $tokens;
+
+    /** @var int */
+    private $constructorIndex;
+
+    public function __construct(Tokens $tokens, int $constructorIndex)
+    {
+        $this->tokens = $tokens;
+        $this->constructorIndex = $constructorIndex;
+    }
+
+    /**
+     * @return array<int, string>
+     */
+    public function getConstructorPromotableParameters(): array
+    {
+        /** @var int $openParenthesis */
+        $openParenthesis = $this->tokens->getNextTokenOfKind($this->constructorIndex, ['(']);
+
+        $closeParenthesis = $this->tokens->findBlockEnd(Tokens::BLOCK_TYPE_PARENTHESIS_BRACE, $openParenthesis);
+
+        $constructorPromotableParameters = [];
+        for ($index = $openParenthesis + 1; $index < $closeParenthesis; $index++) {
+            if (!$this->tokens[$index]->isGivenKind(\T_VARIABLE)) {
+                continue;
+            }
+
+            /** @var int $typeIndex */
+            $typeIndex = $this->tokens->getPrevMeaningfulToken($index);
+            if ($this->tokens[$typeIndex]->equalsAny(['(', ',', [\T_CALLABLE]])) {
+                continue;
+            }
+
+            /** @var int $visibilityIndex */
+            $visibilityIndex = $this->tokens->getPrevTokenOfKind(
+                $index,
+                [
+                    '(',
+                    ',',
+                    [CT::T_CONSTRUCTOR_PROPERTY_PROMOTION_PRIVATE],
+                    [CT::T_CONSTRUCTOR_PROPERTY_PROMOTION_PROTECTED],
+                    [CT::T_CONSTRUCTOR_PROPERTY_PROMOTION_PUBLIC],
+                ]
+            );
+            if (!$this->tokens[$visibilityIndex]->equalsAny(['(', ','])) {
+                continue;
+            }
+
+            $constructorPromotableParameters[$index] = \substr($this->tokens[$index]->getContent(), 1);
+        }
+
+        return $constructorPromotableParameters;
+    }
+
+    /**
+     * @return array<string, int>
+     */
+    public function getConstructorPromotableAssignments(): array
+    {
+        /** @var int $openParenthesis */
+        $openParenthesis = $this->tokens->getNextTokenOfKind($this->constructorIndex, ['(']);
+        $closeParenthesis = $this->tokens->findBlockEnd(Tokens::BLOCK_TYPE_PARENTHESIS_BRACE, $openParenthesis);
+
+        /** @var int $openBrace */
+        $openBrace = $this->tokens->getNextTokenOfKind($closeParenthesis, ['{']);
+        $closeBrace = $this->tokens->findBlockEnd(Tokens::BLOCK_TYPE_CURLY_BRACE, $openBrace);
+
+        $variables = [];
+        $properties = [];
+        $propertyToVariableMap = [];
+
+        for ($index = $openBrace + 1; $index < $closeBrace; $index++) {
+            if (!$this->tokens[$index]->isGivenKind(\T_VARIABLE)) {
+                continue;
+            }
+
+            /** @var int $semicolonIndex */
+            $semicolonIndex = $this->tokens->getNextMeaningfulToken($index);
+            if (!$this->tokens[$semicolonIndex]->equals(';')) {
+                continue;
+            }
+
+            $propertyIndex = $this->getPropertyIndex($index);
+            if ($propertyIndex === null) {
+                continue;
+            }
+
+            $properties[$propertyIndex] = $this->tokens[$propertyIndex]->getContent();
+            $variables[$index] = \substr($this->tokens[$index]->getContent(), 1);
+            $propertyToVariableMap[$propertyIndex] = $index;
+        }
+
+        foreach ($this->getDuplicatesIndices($properties) as $duplicate) {
+            unset($variables[$propertyToVariableMap[$duplicate]]);
+        }
+
+        foreach ($this->getDuplicatesIndices($variables) as $duplicate) {
+            unset($variables[$duplicate]);
+        }
+
+        return \array_flip($variables);
+    }
+
+    private function getPropertyIndex(int $index): ?int
+    {
+        /** @var int $assignmentIndex */
+        $assignmentIndex = $this->tokens->getPrevMeaningfulToken($index);
+        if (!$this->tokens[$assignmentIndex]->equals('=')) {
+            return null;
+        }
+
+        /** @var int $propertyIndex */
+        $propertyIndex = $this->tokens->getPrevMeaningfulToken($assignmentIndex);
+
+        /** @var int $objectOperatorIndex */
+        $objectOperatorIndex = $this->tokens->getPrevMeaningfulToken($propertyIndex);
+
+        /** @var int $thisIndex */
+        $thisIndex = $this->tokens->getPrevMeaningfulToken($objectOperatorIndex);
+        if (!$this->tokens[$thisIndex]->equals([\T_VARIABLE, '$this'], false)) {
+            return null;
+        }
+
+        /** @var int $prevThisIndex */
+        $prevThisIndex = $this->tokens->getPrevMeaningfulToken($thisIndex);
+        if (!$this->tokens[$prevThisIndex]->equalsAny(['{', ';'])) {
+            return null;
+        }
+
+        return $propertyIndex;
+    }
+
+    /**
+     * @param array<int, string> $array
+     *
+     * @return array<int>
+     */
+    private function getDuplicatesIndices(array $array): array
+    {
+        $duplicates = [];
+        $values = [];
+        foreach ($array as $key => $value) {
+            if (isset($values[$value])) {
+                $duplicates[$values[$value]] = $values[$value];
+                $duplicates[$key] = $key;
+            }
+            $values[$value] = $key;
+        }
+
+        return $duplicates;
+    }
+}
---
+++ tests/Analyzer/Analysis/ConstructorAnalysisTest.php
@@ -0,0 +1,139 @@
+<?php
+
+/*
+ * This file is part of PHP CS Fixer: custom fixers.
+ *
+ * (c) 2018 Kuba Werłos
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Analyzer\Analysis;
+
+use PhpCsFixer\Tokenizer\Tokens;
+use PhpCsFixerCustomFixers\Analyzer\Analysis\ConstructorAnalysis;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ *
+ * @covers \PhpCsFixerCustomFixers\Analyzer\Analysis\ConstructorAnalysis
+ */
+final class ConstructorAnalysisTest extends TestCase
+{
+    /**
+     * @param array<int, string> $expected
+     *
+     * @dataProvider provideGettingConstructorPromotableParametersCases
+     */
+    public function testGettingConstructorPromotableParameters(array $expected, string $code): void
+    {
+        $tokens = Tokens::fromCode($code);
+        $analysis = new ConstructorAnalysis($tokens, 11);
+
+        self::assertSame($expected, $analysis->getConstructorPromotableParameters());
+    }
+
+    /**
+     * @return iterable<array{array<int, string>, string}>
+     */
+    public static function provideGettingConstructorPromotableParametersCases(): iterable
+    {
+        yield 'simple parameters' => [
+            [15 => 'a', 20 => 'b', 25 => 'i'],
+            '<?php class Foo {
+                public function __construct(array $a, bool $b, int $i) {}
+            }',
+        ];
+
+        yield 'parameters without types are not supported' => [
+            [21 => 'i'],
+            '<?php class Foo {
+                public function __construct($noType1, $noType2, int $i, $noType3) {}
+            }',
+        ];
+
+        yield 'callable is not supported' => [
+            [15 => 'a', 20 => 'b', 35 => 'i'],
+            '<?php class Foo {
+                public function __construct(array $a, bool $b, callable $c1, CALLABLE $c1, int $i) {}
+            }',
+        ];
+
+        yield 'some already promoted' => [
+            [22 => 'b', 39 => 's'],
+            '<?php class Foo {
+                public function __construct(public array $a, bool $b, protected ?Bar\Baz\Qux $q, string $s, private OtherType $t) {}
+            }',
+        ];
+    }
+
+    /**
+     * @param array<int, string> $expected
+     *
+     * @dataProvider provideGettingConstructorPromotableAssignmentsCases
+     */
+    public function testGettingConstructorPromotableAssignments(array $expected, string $code): void
+    {
+        $tokens = Tokens::fromCode($code);
+        $analysis = new ConstructorAnalysis($tokens, 11);
+
+        self::assertSame($expected, $analysis->getConstructorPromotableAssignments());
+    }
+
+    /**
+     * @return iterable<array{array<string, int>, string}>
+     */
+    public static function provideGettingConstructorPromotableAssignmentsCases(): iterable
+    {
+        yield 'simple assignments' => [
+            ['x' => 30,   'y' => 39,  'z' => 48],
+            '<?php class Foo {
+                public function __construct($x, $y, $z) {
+                    $this->a = $x;
+                    $this->b = $y;
+                    $this->c = $z;
+                }
+            }',
+        ];
+
+        yield 'duplicated assigned parameters' => [
+            ['x' => 30, 'z' => 59],
+            '<?php class Foo {
+                public function __construct($x, $y, $z) {
+                    $this->a = $x;
+                    $this->b = $y;
+                    $this->c = $y; // $y is assigned for 2nd time
+                    $this->d = $z;
+                }
+            }',
+        ];
+
+        yield 'duplicated assigned properties' => [
+            ['x' => 30],
+            '<?php class Foo {
+                public function __construct($x, $y, $z) {
+                    $this->a = $x;
+                    $this->b = $y;
+                    $this->b = $z; // $this->b is assigned for 2nd time
+                }
+            }',
+        ];
+
+        yield 'not allowed assignment' => [
+            ['e' => 86],
+            '<?php class Foo {
+                public function __construct($a, $b, $c, $d, $e) {
+                    $this->a = $a + 1;
+                    $notThis->b = $b;
+                    $this->c = 1 + $c;
+                    $this->d = $this->d2 = $d;
+                    $this->e = $e;
+                }
+            }',
+        ];
+    }
+}
