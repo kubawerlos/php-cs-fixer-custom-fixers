be31f2cc26d63bcfabe772fb7f9c8ebdde41227a
Kuba Werłos <werlos@gmail.com>
2021-10-27 21:38:53 +0200

    Add FunctionAnalyzer (#634)

---
+++ src/Analyzer/Analysis/ArgumentAnalysis.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of PHP CS Fixer: custom fixers.
+ *
+ * (c) 2018 Kuba Werłos
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace PhpCsFixerCustomFixers\Analyzer\Analysis;
+
+/**
+ * @internal
+ */
+final class ArgumentAnalysis
+{
+    /** @var int */
+    private $startIndex;
+
+    /** @var int */
+    private $endIndex;
+
+    /** @var bool */
+    private $isConstant;
+
+    public function __construct(int $startIndex, int $endIndex, bool $isConstant)
+    {
+        $this->startIndex = $startIndex;
+        $this->endIndex = $endIndex;
+        $this->isConstant = $isConstant;
+    }
+
+    public function getStartIndex(): int
+    {
+        return $this->startIndex;
+    }
+
+    public function getEndIndex(): int
+    {
+        return $this->endIndex;
+    }
+
+    public function isConstant(): bool
+    {
+        return $this->isConstant;
+    }
+}
