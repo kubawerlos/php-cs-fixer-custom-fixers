cb98e1bcd029910d9cef4881697a80f1f5676664
Kuba Werłos <werlos@gmail.com>
2022-05-03 08:44:41 +0200

    Update

--- .dev-tools/autoload.php
+++ .dev-tools/autoload.php
@@ -19,20 +19,23 @@ foreach ($autoloads as $autoload) {
     }
 }
 
-if (!interface_exists('PhpCsFixer\\Fixer\\FixerInterface')) {
-    $phars = [
-        __DIR__ . '/../vendor/php-cs-fixer/shim/php-cs-fixer',
-        __DIR__ . '/vendor/php-cs-fixer/shim/php-cs-fixer',
-    ];
-    foreach ($phars as $phar) {
-        if (file_exists($phar)) {
-            $pharLoaded = Phar::loadPhar($phar, 'php-cs-fixer.phar');
-            if (!$pharLoaded) {
-                exit(sprintf('Phar "%s" not loaded!' . PHP_EOL, $phar));
-            }
+if (interface_exists('PhpCsFixer\\Fixer\\FixerInterface')) {
+    return;
+}
 
-            require_once 'phar://php-cs-fixer.phar/vendor/autoload.php';
-            break;
+$phars = [
+    __DIR__ . '/../vendor/php-cs-fixer/shim/php-cs-fixer',
+    __DIR__ . '/vendor/php-cs-fixer/shim/php-cs-fixer',
+];
+foreach ($phars as $phar) {
+    if (file_exists($phar)) {
+        $pharLoaded = Phar::loadPhar($phar, 'php-cs-fixer.phar');
+        if (!$pharLoaded) {
+            exit(sprintf('Phar "%s" not loaded!' . PHP_EOL, $phar));
         }
+
+        require_once 'phar://php-cs-fixer.phar/vendor/autoload.php';
+
+        return;
     }
 }
