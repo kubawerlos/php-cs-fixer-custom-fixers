1eef5bdde2eb87c25488d921f67ce258f8d3070f
Kuba Werłos <werlos@gmail.com>
2023-03-02 19:58:47 +0100

    Fix ReadmeCommand deprecation warning (#882)

--- .dev-tools/src/Readme/ReadmeCommand.php
+++ .dev-tools/src/Readme/ReadmeCommand.php
@@ -24,6 +24,7 @@ use PhpCsFixerCustomFixers\Fixer\DataProviderStaticFixer;
 use PhpCsFixerCustomFixers\Fixers;
 use SebastianBergmann\Diff\Differ;
 use SebastianBergmann\Diff\Output\StrictUnifiedDiffOutputBuilder;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -32,6 +33,7 @@ use Symfony\Component\Process\Process;
 /**
  * @internal
  */
+#[AsCommand(name: 'readme')]
 final class ReadmeCommand extends Command
 {
     protected static $defaultName = 'readme';
