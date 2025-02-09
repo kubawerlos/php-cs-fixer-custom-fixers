2a4775d7af87f12eccf76d696a1930ca803f2ea5
Kuba Werłos <werlos@gmail.com>
2025-02-09 22:51:59 +0100

    Add ClassConstantUsageFixer

---
+++ tests/Fixer/ClassConstantUsageFixerTest.php
@@ -0,0 +1,81 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of PHP CS Fixer: custom fixers.
+ *
+ * (c) 2018 Kuba Werłos
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Tests\Fixer;
+
+/**
+ * @internal
+ *
+ * @covers \PhpCsFixerCustomFixers\Fixer\ClassConstantUsageFixer
+ */
+final class ClassConstantUsageFixerTest extends AbstractFixerTestCase
+{
+    public function testIsRisky(): void
+    {
+        self::assertRiskiness(false);
+    }
+
+    /**
+     * @dataProvider provideFixCases
+     */
+    public function testFix(string $expected, ?string $input = null): void
+    {
+        $this->doTest($expected, $input);
+    }
+
+    /**
+     * @return iterable<array{0: string, 1?: string}>
+     */
+    public static function provideFixCases(): iterable
+    {
+        yield 'multiple constants with the same value' => [
+            <<<'PHP'
+                <?php
+                class Foo
+                {
+                    public const BAR = 'a';
+                    public const BAZ = 'a';
+                    public function f()
+                    {
+                        return 'a';
+                    }
+                }
+                PHP,
+        ];
+
+        yield 'constants all over the class' => [
+            <<<'PHP'
+                <?php
+                class Foo
+                {
+                    public const BAR = 'bar';
+                    public function f()
+                    {
+                        return self::BAR . self::BAZ;
+                    }
+                    public const BAZ = 'baz';
+                }
+                PHP,
+            <<<'PHP'
+                <?php
+                class Foo
+                {
+                    public const BAR = 'bar';
+                    public function f()
+                    {
+                        return 'bar' . 'baz';
+                    }
+                    public const BAZ = 'baz';
+                }
+                PHP,
+        ];
+    }
+}
